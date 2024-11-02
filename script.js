b3754755cc511dd2a715f52de5020076ccb50e1c
Gici9 <kapalsumatera@gmail.com>
2024-11-02 09:48:17 +0700

    Update script.js
    Signed-off-by: Gici9 <kapalsumatera@gmail.com>

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function submitForm() {
 // Fungsi untuk hitung mundur
 function countdown() {
     const targetTime = new Date();
-    targetTime.setHours(13, 14, 0, 0); // Sesuaikan waktu
+    targetTime.setHours(9, 50, 0, 0); // Sesuaikan waktu
 
     const interval = setInterval(() => {
         const now = new Date();
